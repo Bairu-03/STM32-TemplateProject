f4e5a36062cb23f7925ec47bc2a02b08bcd09e08
Bairu <bairufxm@foxmail.com>
2024-05-16 18:42:57 +0800

    修改寻迹模块IO口（改为PB3-PB7）

--- Hardware/InfTrack.c
+++ Hardware/InfTrack.c
@@ -8,13 +8,15 @@
  */
 void InfTracker_Init(void)
 {
-    RCC_APB2PeriphClockCmd(RCC_Periph, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);  // 开启复用时钟
+    GPIO_PinRemapConfig(GPIO_Remap_SWJ_JTAGDisable, ENABLE);  // 关闭JTAG，保留SWD，释放PA15、PB3、PB4作为普通IO口
 
+    RCC_APB2PeriphClockCmd(RCC_Periph, ENABLE);
     GPIO_InitTypeDef GPIO_InitStructure;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING; // 浮空输入
     GPIO_InitStructure.GPIO_Pin = ITOUT5 | ITOUT4 | ITOUT3 | ITOUT2 | ITOUT1;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_Init(GPIOA, &GPIO_InitStructure);
+    GPIO_Init(InfTGPIO, &GPIO_InitStructure);
 }
 
 /**
@@ -28,7 +30,7 @@ uint8_t Get_InfTdata(void)
     for (i = 0; i < 5; i++)
     {
         InfTdata = InfTdata << 1;
-        InfTdata = InfTdata + GPIO_ReadInputDataBit(GPIO, ITOUT1 >> i);
+        InfTdata = InfTdata + GPIO_ReadInputDataBit(InfTGPIO, ITOUT1 >> i);
     }
     return InfTdata;
 }
--- Hardware/InfTrack.h
+++ Hardware/InfTrack.h
@@ -2,19 +2,19 @@
 #define __INFTRACK_H
 
 // 寻迹模块IO口定义
-#define RCC_Periph RCC_APB2Periph_GPIOA
-#define GPIO GPIOA
-#define ITOUT1 GPIO_Pin_12
-#define ITOUT2 GPIO_Pin_11
-#define ITOUT3 GPIO_Pin_10
-#define ITOUT4 GPIO_Pin_9
-#define ITOUT5 GPIO_Pin_8
+#define RCC_Periph RCC_APB2Periph_GPIOB
+#define InfTGPIO GPIOB
+#define ITOUT1 GPIO_Pin_7
+#define ITOUT2 GPIO_Pin_6
+#define ITOUT3 GPIO_Pin_5
+#define ITOUT4 GPIO_Pin_4
+#define ITOUT5 GPIO_Pin_3
 
-#define IT_M() GPIO_ReadInputDataBit(GPIO, ITOUT3)  // 五路红外寻迹-中间
-#define IT_L1() GPIO_ReadInputDataBit(GPIO, ITOUT2) // 五路红外寻迹-左1
-#define IT_L2() GPIO_ReadInputDataBit(GPIO, ITOUT1) // 五路红外寻迹-左2
-#define IT_R1() GPIO_ReadInputDataBit(GPIO, ITOUT4) // 五路红外寻迹-右1
-#define IT_R2() GPIO_ReadInputDataBit(GPIO, ITOUT5) // 五路红外寻迹-右2
+#define IT_M() GPIO_ReadInputDataBit(InfTGPIO, ITOUT3)  // 五路红外寻迹-中间
+#define IT_L1() GPIO_ReadInputDataBit(InfTGPIO, ITOUT2) // 五路红外寻迹-左1
+#define IT_L2() GPIO_ReadInputDataBit(InfTGPIO, ITOUT1) // 五路红外寻迹-左2
+#define IT_R1() GPIO_ReadInputDataBit(InfTGPIO, ITOUT4) // 五路红外寻迹-右1
+#define IT_R2() GPIO_ReadInputDataBit(InfTGPIO, ITOUT5) // 五路红外寻迹-右2
 
 void InfTracker_Init(void);
 uint8_t Get_InfTdata(void);
